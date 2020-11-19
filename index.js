f8d259a03da2b8735b11932d44049b4451155a75
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-11-19 09:06:56 -0500

    required and usedbody-parser

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Models = require("./models.js");
 
 const Movies = Models.Movie;
 const Users = Models.User;
-const Genres = Models.Genre;
-const Directors = Models.Director;
 
 mongoose.connect("mongodb://localhost:27017/myFlixDB", {
   useNewUrlParser: true,
@@ -13,12 +11,14 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", {
 });
 
 const express = require("express"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 // All movies
 let movies = [];
@@ -105,7 +105,7 @@ app.get("/users/:Username", (req, res) => {
     });
 });
 
-//Add a user
+//Allow user to register
 app.post("/users", (req, res) => {
   Users.findOne({ Username: req.body.Username })
     .then(user => {
@@ -159,7 +159,7 @@ app.put("/users/:Username", (req, res) => {
 
 //Add a movie to a users list of favourites
 app.post("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.fineOneAndUpdate(
+  Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
       $push: { FavouriteMovies: req.params.MovieID }
@@ -177,8 +177,8 @@ app.post("/users/:Username/Movies/:MovieID", (req, res) => {
 });
 
 //Remove a movie from a users list of favourites
-app.post("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.fineOneAndUpdate(
+app.delete("/users/:Username/Movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
       $pull: { FavouriteMovies: req.params.MovieID }
@@ -197,7 +197,7 @@ app.post("/users/:Username/Movies/:MovieID", (req, res) => {
 
 // Delete a user by username
 app.delete("/users/:Username", (req, res) => {
-  Users.fineOneAndRemove({ Username: req.params.Username })
+  Users.findOneAndRemove({ Username: req.params.Username })
     .then(user => {
       if (!user) {
         res.status(400).send(req.params.Username + " was not found.");
@@ -221,88 +221,3 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-
-// -----------------------------------------------------------
-
-// //Dependencies
-// const express = require("express"),
-// morgan = require("morgan");
-//
-// const app = express();
-//
-// app.use(morgan("common"));
-// app.use(express.static("public"));
-//
-// // All movies
-// let movies = [];
-//
-// //Favourite movies
-// let myFavourites = [];
-//
-// //Home page
-// app.get("/", (req, res) => {
-// res.send("Welcome to myFlix!");
-// });
-//
-// // List of data of all movies
-// app.get("/movies", (req, res) => {
-// // res.json(movies);
-// res.send("Succesful GET request to retrieve all movies.");
-// });
-//
-// //Loads json of myFavourites object
-// app.get("/movies/myFavourites", (req, res) => {
-// // res.json(myFavourites);
-// res.send("Succesful GET request to retrieve myFavourite movies.");
-// });
-//
-// // Gets data about a single movie by title
-// app.get("/movies/:title", (req, res) => {
-// res.send("Succesful GET request returning data on a single movie.");
-// });
-//
-// // Gets data about a genre by title
-// app.get("/movies/genre/:title", (req, res) => {
-// res.send("Succesful GET request returning data of a genre by title.");
-// });
-//
-// // Gets data about director by title
-// app.get("/movies/director/:title", (req, res) => {
-// res.send("Succesful GET request returning data on a director by title.");
-// });
-//
-// // Post new user registration
-// app.post("/user", (req, res) => {
-// res.send("Succesful POST request for user registration.");
-// });
-//
-// // Updates user info (username only for now)
-// app.put("/user/:userName", (req, res) => {
-// res.send("Succesful PUT request to update username.");
-// });
-//
-// // Post a movie to myFavourites
-// app.post("/user/:newFavourite", (req, res) => {
-// res.send("Succesful POST request to add a movie to myFavourites.");
-// });
-//
-// // Delete a movie from myFavourites
-// app.delete("/user/:myFavourites", (req, res) => {
-// res.send("Succesful DELETE request to remove a movie from myFavourites.");
-// });
-//
-// // Delete user account
-// app.delete("/user/account/:userName", (req, res) => {
-// res.send("Succesful DELETE request to delete an account.");
-// });
-//
-// //Logs every time you load a page
-// app.listen(8080, () => {
-// console.log("Your app is listening on port 8080.");
-// });
-//
-// //Error handler
-// app.use((err, req, res, next) => {
-// console.error(err.stack);
-// res.status(500).send("Something broke!");
-// });
