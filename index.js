d363ff909ac9b02d218a6f7e24108ea079ef9dc7
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-11-21 10:08:02 -0500

    commit name

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post(
   ],
   (req, res) => {
     // check the validation object for errors
-    let error = validationResult(req);
+    let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
